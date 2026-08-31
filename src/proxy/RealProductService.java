11c48c347cd02657cc8f35d7a1314816f9adb84e
AyanMansuri10 <ayanmansuri0018@gmail.com>
2026-08-31 11:31:12 +0530

    Merge remote changes

--- src/proxy/RealProductService.java
+++ src/proxy/RealProductService.java
@@ -5,7 +5,6 @@ import model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RealProductService implements ProductService{
